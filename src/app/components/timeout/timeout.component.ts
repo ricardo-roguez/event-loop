5a937383b577a4cf7c3d5a25f53af5ffcdf2a0b1
Ricardo Rodríguez <120378561+ricardo-roguez@users.noreply.github.com>
2023-05-06 13:10:08 +0200

    feart: add inherit feature

--- src/app/components/timeout/timeout.component.ts
+++ src/app/components/timeout/timeout.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import { MySuperClass } from '../my-super-class';
 
 @Component({
   selector: 'app-timeout',
@@ -6,11 +7,20 @@ import {Component, Input} from '@angular/core';
   templateUrl: './timeout.component.html',
   styleUrls: ['./timeout.component.scss']
 })
-export class TimeoutComponent {
+export class TimeoutComponent extends MySuperClass<string> {
   @Input() inputOne: string = '';
 
   constructor() {
+    super();
     console.log('En el timeoutComponent pero fuera del timeOut: ', this.inputOne);
-    setTimeout(() => console.log('En el timeoutComponent pero dentro del timeOut component: ', this.inputOne));
+    this.setData(this.inputOne);
+    this.logValue();
+
+
+    // setTimeout(() => {
+    //   console.log('En el timeoutComponent pero dentro del timeOut: ', this.inputOne);
+    //   this.setData(this.inputOne);
+    //   this.logValue();
+    // }, 0);
   }
 }
