37802ecb0eaa97275e37c49a91e0f24bd0189618
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2023-05-08 08:30:40 +0200

    ref: rename function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,23 +13,24 @@ export class AppComponent {
 
   initTimeoutDemo(): void {
     const dateAtInit = new Date().getSeconds();
-    console.log('Antes del timeout:', this.getFormattedDate());
+    
+    console.log('Antes del timeout:', this.getFormattedTime());
 
-    setTimeout(() => console.log('En el timeout:', this.getFormattedDate()), 0);
+    setTimeout(() => console.log('En el timeout:', this.getFormattedTime()), 3000);
 
-    console.log('Después del timeout:', this.getFormattedDate());
+    console.log('Después del timeout:', this.getFormattedTime());
 
     while(true) {
       const now = new Date().getSeconds();
       if(now - dateAtInit >= 2) {
-        console.log('Sistema parado por 2 segundos', this.getFormattedDate());
+        console.log('Sistema parado por 2 segundos', this.getFormattedTime());
         break;
       }
     }
-    console.log('Después del loop:', this.getFormattedDate());
+    console.log('Después del loop:', this.getFormattedTime());
   }
 
-  private getFormattedDate(): string {
+  private getFormattedTime(): string {
     const now = new Date();
     return `Es el segundo ${now.getSeconds()} con ${now.getMilliseconds()} milisegundos`;
   }
