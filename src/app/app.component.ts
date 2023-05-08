694e09d38508b76d6c9bed0e4d94ef8209b56820
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2023-05-08 08:14:46 +0200

    feat: prepare code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   inputOne = 'Valor del input One';
   constructor() {
-    // this.initTimeoutDemo();
+    this.initTimeoutDemo();
   }
 
   initTimeoutDemo(): void {
